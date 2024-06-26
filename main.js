ab6211caa40da74ec89bf76ee9e60fad41fd067e
suhaib9173 <suhaibsaleh452@gmail.com>
2024-06-26 14:41:15 +0300

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-var data="";
-    data+="<li>Taboo Burger Bar</li>";
-    data+="<li>Zest Restaurant</li>";
-    data+="<li>Pronto</li>";
-document.querySelector("ul").innerHTML=data;
+var exchangeForm=document.querySelector(".exchangeForm");
+
+exchangeForm.onsubmit=function(e){
+e.preventDefault();
+var elements=e.target.elements;
+console.log(elements[1].value);
+
+if(elements[1].value=="dollar"){
+    elements[0].value*=3.5;
+}else if(elements[1].value=="dinar"){
+    elements[0].value*=4.8;
+}else{
+    elements[0].value*=1;
+}
+    console.log(elements[0].value);
+   
+
+}
